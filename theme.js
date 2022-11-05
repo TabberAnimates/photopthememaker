ecf654d5adb6d9a66a292eb005b3a758ff7eca43
TabberAnimates <94705401+TabberAnimates@users.noreply.github.com>
2022-11-05 00:25:59 -0400

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,4 +1,36 @@
-import theme from './preset.json' assert {type: 'json'};
+//Color Data
+const theme = `{
+  "leftSidebarColor": "#300d20",
+  "pageColor": "#17060f80", 
+  "contentColor": "#290b1b", 
+  "contentColor2": "#2e0c1e", 
+  "contentColor3": "#380f25", 
+  "borderColor": "#42122c", 
+  "fontColor": "#ffffff", 
+  "themeColor": "#FF42A7"
+}`
+//CSS Animation Data
+const animation = `body { 
+  background-color: #95234a; 
+  animation-name: example; 
+  animation-duration: 4s; 
+  animation-iteration-count: 1.7976931348623157e+309; 
+}
+@keyframes example { 
+  0%   {background-color: #95234a;} 
+  25%  {background-color: #4b87bc;} 
+  50%  {background-color: #1e183b;} 
+  75% {background-color: #3f7ca9;} 
+  100%   {background-color: #95234a;} 
+}`
+let prompt1 = prompt("Would you like to enter a theme code? (Y/N)");
+if (!prompt1 = "N") {
+  let prompt2 = prompt("Enter the code here:");
+  const output = prompt2.split("˃");
+  let theme = output[0];
+  let animation = output[1];
+}
+
 preset = JSON.parse(theme);
 
 var e = document.getElementsByTagName('html')[0];
@@ -17,4 +49,5 @@ para.innerHTML = str;
 document.body.appendChild(para);
 }
 
-let p = add("style", `body { background-color: #95234a; animation-name: example; animation-duration: 4s; animation-iteration-count: 1.7976931348623157e+309; } @keyframes example { 0%   {background-color: #95234a;} 25%  {background-color: #4b87bc;} 50%  {background-color: #1e183b;} 75% {background-color: #3f7ca9;} 100%   {background-color: #95234a;} } .pageHolder { background-color: var(--pageColor); border-radius: 12px; } .stickyContainer { position: sticky; top: 8px; z-index: 500; border-radius: 12px; box-shadow: 0px 0px 0px var(--pageColor), 0px 0px 0px var(--pageColor); } .sidebar { border-radius: 12px; position: sticky; display: flex; flex-direction: column; box-sizing: border-box; width: 200px; height: calc(100vh - 16px); top: 8px; background: var(--pageColor); color: var(--themeColor); font-size: 20px; line-height: 20px; font-family: var(--secondFont); font-weight: bold; }`);
+let betterStyle = add("style", `.pageHolder { background-color: var(--pageColor); border-radius: 12px; } .stickyContainer { position: sticky; top: 8px; z-index: 500; border-radius: 12px; box-shadow: 0px 0px 0px var(--pageColor), 0px 0px 0px var(--pageColor); } .sidebar { border-radius: 12px; position: sticky; display: flex; flex-direction: column; box-sizing: border-box; width: 200px; height: calc(100vh - 16px); top: 8px; background: var(--pageColor); color: var(--themeColor); font-size: 20px; line-height: 20px; font-family: var(--secondFont); font-weight: bold; }`);
+let p = add("style", animation);
