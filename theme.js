273049d59873cd4d43f4f27a7dae5e54bb7ab1cb
TabberAnimates <94705401+TabberAnimates@users.noreply.github.com>
2022-11-06 13:03:12 -0500

    Update theme.js

--- theme.js
+++ theme.js
@@ -23,12 +23,21 @@ const animation = `body {
   75% {background-color: #3f7ca9;} 
   100%   {background-color: #95234a;} 
 }`
+
+function loadCode(str) {
+  const output = str.split("˃");
+  let theme = output[0];
+  let animation = output[1];
+  alert("A code has been loaded!");
+}
+
 let prompt1 = prompt("Would you like to enter a theme code? (Y/N)");
 if (prompt1.toLowerCase().includes("y")) {
   let prompt2 = prompt("Enter the code here:");
-  const output = prompt2.split("˃");
-  let theme = output[0];
-  let animation = output[1];
+  loadCode(prompt2);
+} else {
+  let code = `["#300d20","#17060f80","#290b1b","#2e0c1e","#380f25","#42122c","ffffff","ff42a7"]˃body { background-color: #95234a; animation-name: example; animation-duration: 4s; animation-iteration-count: 1.7976931348623157e+309; } @keyframes example { 0%   {background-color: #95234a;} 25%  {background-color: #4b87bc;} 50%  {background-color: #1e183b;} 75% {background-color: #3f7ca9;} 100%   {background-color: #95234a;} }`;
+  loadCode(code);
 }
 
 var e = document.getElementsByTagName('html')[0];
